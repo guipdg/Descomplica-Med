8af37f3e21dbab8b29ffa986cb3bb997e2bccd29
am4ndioc4 <amandaamorim.ctt@gmail.com>
2024-09-28 00:11:19 -0300

    @amandioca | feat: adiciona navbar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+const colors = {
+  babyBlue: '#ade0ff',
+  blue: '#1447FF',
+  gray: '#7d7d7d',
+  white: '#e4eaed',
+  black: '#262829'
+}
+
+const appName = 'Descomplica Med'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+const NavBar = () => {
+  const navbarStyle = { backgroundColor: colors.blue }
+  const logoIconStyle = { color: colors.babyBlue, paddingRight: 8 }
+  const brandLinkStyle = { color: colors.babyBlue, fontWeight: 500, fontSize: 22 }
+
+  return (
+    <nav class='navbar' style={navbarStyle}>
+      <div class='container-fluid'>
+        <a class='navbar-brand'
+          href='#'
+          style={brandLinkStyle}>
+          <i class='fa-solid fa-comment-medical fa-lg'
+            style={logoIconStyle}></i>
+          {appName}
+        </a>
+      </div>
+    </nav>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
