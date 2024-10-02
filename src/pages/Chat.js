7e20ff4d14f3e6fad0fc1783d1b5bc2776fb5dd2
am4ndioc4 <amandaamorim.ctt@gmail.com>
2024-10-01 23:00:22 -0300

    @amandioca | feat: cria paginas Chat, Home, Login e Signup.js

---
+++ src/pages/Chat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+    return (
+        <div>
+            <h1>Aqui é a página de CHAT!</h1>
+        </div>
+    )
+}
+
+export default Chat;
---
+++ src/pages/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+    return (
+        <div>
+            <h1>Aqui é a página de SIGNUP!</h1>
+        </div>
+    )
+}
+
+export default Signup;
