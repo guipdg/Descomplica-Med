4b7b96ecce77e4432766acd136d6bdb373d2cd92
am4ndioc4 <amandaamorim.ctt@gmail.com>
2024-10-01 22:25:10 -0300

    @amandioca | feat: cria UnauthNavbar em Navbar.js e adiciona logo-white.svg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React from 'react';
 import logo from'../assets/svgs/logo.svg'
+import logoWhite from'../assets/svgs/logo-white.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const logoContainerStyle = {justifyContent: 'center'}
+
 const AuthNavbar = ({colorPalette}) => {
-    const navbarStyle = { backgroundColor: colorPalette.grayMedium}
-    const logoContainerStyle = {justifyContent: 'center'}
+    const navbarStyle = { backgroundColor: colorPalette.grayMedium, height: 66}
   
     return (
-      <nav class="navbar" style={navbarStyle}>
-        <div class="container" style={logoContainerStyle}>
-          <a class="navbar-brand" href="#">
-            <img src={logo} alt="Descomplica Med" width="40"/>
+      <nav class='navbar' style={navbarStyle}>
+        <div class='container' style={logoContainerStyle}>
+          <a class='navbar-brand' href='#'>
+            <img src={logo} alt='Descomplica Med' width='40'/>
           </a>
         </div>
       </nav>
     )
 }
 
-export {AuthNavbar};
+const UnauthNavbar = ({colorPalette}) => {
+  const navbarStyle = { 
+    background: `linear-gradient(to bottom right, ${colorPalette.primaryBlue}, ${colorPalette.secondaryBlue})`, 
+    height: 66}
+  
+  return (
+    <nav class='navbar' style={navbarStyle}>
+      <div class='container' style={logoContainerStyle}>
+        <a class='navbar-brand' href='#' style={{ display: 'flex', alignItems: 'center', fontWeight: 400, fontSize: 28, color: '#FFF'}}>
+          <img style={{marginRight: 10}} src={logoWhite} alt='Descomplica Med' width='40'/>
+          Descomplica Med
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export {AuthNavbar, UnauthNavbar};
